323f8c1492acdadd1b9895262824d534c05dbff0
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-05-25 15:02:10 +0200

    statless widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,15 @@ void main() {
           backgroundColor: Colors.brown[400],
           centerTitle: true,
         ),
-        body: const Text("Hello!")),
+        body: const Home()),
   ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Widget Hello!");
+  }
 }
