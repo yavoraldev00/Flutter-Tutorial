5d8002c5c5e3604b71fb24f2d6d4a2b51b7f4093
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-06-22 17:56:39 +0200

    styling comments

--- lib/styled_body_text.dart
+++ lib/styled_body_text.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class StyledBodyText extends StatelessWidget {
   // custom font style
-  const StyledBodyText(this.text, {super.key});
+  const StyledBodyText(this.text, {super.key}); // takes text as parameter
 
-  final String text;
+  final String text; // text parameter
 
   @override
   Widget build(BuildContext context) {
     return Text(text,
         style: TextStyle(
-            color: Colors.brown[900],
-            fontWeight: FontWeight.bold,
-            fontSize: 18));
+            color: Colors.brown[900], // font color
+            fontWeight: FontWeight.bold, // font weight / boldness
+            fontSize: 18)); // font size
   }
 }
--- lib/styled_button.dart
+++ lib/styled_button.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 
 class StyledButton extends StatelessWidget {
   // custom button style
+
+  // takes function and Widget as parameter
   const StyledButton({super.key, required this.onPressed, required this.child});
 
-  final Widget child;
-  final void Function() onPressed;
+  final Widget child; // widget, used for button child
+  final void Function() onPressed; // function that executes onPress
 
   @override
   Widget build(BuildContext context) {
+    // button styling
     return TextButton(
         style: TextButton.styleFrom(
-            backgroundColor: Colors.brown[700],
-            foregroundColor: Colors.white,
+            backgroundColor: Colors.brown[700], // button background
+            foregroundColor: Colors.white, // button text color
             shape: const RoundedRectangleBorder(
+                // button shape
                 borderRadius: BorderRadius.all(Radius.circular(5)))),
-        onPressed: onPressed,
-        child: child);
+        onPressed: onPressed, // function executed onPress
+        child: child); // child inside TextButton container
   }
 }
